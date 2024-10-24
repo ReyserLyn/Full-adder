f198543001153317bab00f0501c0bfa5fb8be007
Jorghee <jorgeehuarsaya@gmail.com>
2024-10-23 23:10:57 -0500

    Change the name function

--- frontend/src/lib/sumador-1bit.ts
+++ frontend/src/lib/sumador-1bit.ts
@@ -1,4 +1,4 @@
-export default function FullAdder(a: number, b: number, carry_in : number){
+export default function FullAdder1Bit(a: number, b: number, carry_in : number){
   const sum = a ^ b ^ carry_in;
   const carry_out = (a & b) | (carry_in & (a ^ b));
   return {sum, carry_out};
